eebb8a981c1757754f6102b0ddf301e8e0f8cd34
A K Biplob <akbiplobx@gmail.com>
2026-03-31 03:34:37 +0600

    btn redesign

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -35,7 +35,7 @@ const Banner = () => {
               Explore Products
             </button>
             
-            <button className="flex items-center gap-2 border border-slate-200 px-8 py-3.5 rounded-full font-semibold text-slate-700 hover:bg-slate-50 transition-all">
+            <button className="flex items-center gap-2 border border-purple-500 px-8 py-3.5 rounded-full font-semibold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent hover:bg-slate-50 transition-all">
               <span className="text-[#7C3AED]">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
